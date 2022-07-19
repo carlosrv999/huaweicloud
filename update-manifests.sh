eb44931b597672f5d6c8e3c75b7df5e8cda8dd4a
Carlos Ramirez <carlosrv125@icloud.com>
2022-07-19 10:53:47 -0500

    fixed script

--- update-manifests.sh
+++ update-manifests.sh
@@ -9,6 +9,6 @@ base64pass=$(echo -n $emojivote_dbpasswd | base64)
 echo "$(date) Updating manifests file"
 sed -i "3s/.*/  MYSQL_HOST: ${emoji_ip}/" ./manifests-emojivote/envs/emoji-db-access-configmap.yaml
 sed -i "3s/.*/  MYSQL_HOST: ${vote_ip}/" ./manifests-emojivote/envs/vote-db-access-configmap.yaml
-sed -i "3s/.*/  MYSQL_PASSWD: ${base64pass}/" ./manifests-emojivote/envs/db-access-emoji.yaml
-sed -i "3s/.*/  MYSQL_PASSWD: ${base64pass}/" ./manifests-emojivote/envs/db-access-vote.yaml
+sed -i "4s/.*/  MYSQL_PASSWD: ${base64pass}/" ./manifests-emojivote/envs/db-access-emoji.yaml
+sed -i "4s/.*/  MYSQL_PASSWD: ${base64pass}/" ./manifests-emojivote/envs/db-access-vote.yaml
 echo "$(date) SUCCESS"
